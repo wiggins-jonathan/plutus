2fe49414e5e6fd71bc68008b3c6d367adf3b9b96
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2021-07-31 15:13:06 -0700

    Add ability for plutus to serve ticker data at /p

--- cmd/server.go
+++ cmd/server.go
@@ -8,13 +8,28 @@ import (
 )
 
 func serve() {
-    http.HandleFunc("/p", getDailyPrice)
+    http.HandleFunc("/p", priceHandler)
     fmt.Println("Server listening on port 5000")
     log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
-func getDailyPrice(w http.ResponseWriter, r *http.Request) {
-    // Take in the ticker & call GetPrice()
-    price := getPrice("AMZN")
-    fmt.Fprintf(w, "Price is %g", price)
+// Take in the ticker & call GetPrice()
+func priceHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/p" {
+        http.NotFound(w, r)
+        return
+    }
+    if r.Method != "GET" {
+        http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+
+    ticker := r.URL.Query().Get("ticker")
+    if ticker == "" {
+        http.Error(w, "Please provide ticker=<ticker>", http.StatusBadRequest)
+        return
+    }
+
+    price := getPrice(ticker)
+    fmt.Fprintf(w, "Price for %s is $%g", ticker, price)
 }
